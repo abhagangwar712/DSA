2038450f9539ed89b41fcd9214ca27ca32328099
abhagangwar712 <abhagangwar712@gmail.com>
2025-07-02 23:35:26 +0530

    added new file

---
+++ Binary_search/153_leetcode.cpp
@@ -0,0 +1,20 @@
+// LEETCODE LINK : https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/submissions/1684193101/
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int left = 0, right = nums.size() - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
+};
