bbdaac41f945adfaf7e8d479126b6df82ee700ee
abhagangwar712 <abhagangwar712@gmail.com>
2025-07-03 12:51:36 +0530

    added new file

---
+++ Recursion + Backtracking/46_leetcode.cpp
@@ -0,0 +1,32 @@
+// LEETCODE LINK : https://leetcode.com/problems/permutations/submissions/1684740020/
+
+#include <vector>
+#include <functional>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> result;
+    vector<int> current;
+    vector<bool> used(nums.size(), false);
+
+    function<void()> backtrack = [&]() {
+        if (current.size() == nums.size()) {
+            result.push_back(current);
+            return;
+        }
+        for (int i = 0; i < nums.size(); ++i) {
+            if (used[i]) continue;
+            used[i] = true;
+            current.push_back(nums[i]);
+            backtrack();
+            current.pop_back();
+            used[i] = false;
+        }
+    };
+
+    backtrack();
+    return result;
+    }
+};
